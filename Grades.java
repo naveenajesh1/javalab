9f2a00fcc423ab6f3e1c871b9c4feee5cad8a018
naveenajesh1 <145547512+naveenajesh1@users.noreply.github.com>
2023-09-26 15:26:35 +0530

    Add files via upload

---
+++ Grades.java
@@ -0,0 +1,49 @@
+/*
+ * File        : Grades.java
+ * Description : Print hello world
+ * Author      : Naveen
+ * Version     : 1.0
+ * Date        : 26/09/2023
+ * 
+ */
+package javalab;
+import java.util.Scanner;
+public class Grades {
+	public static void main(String[] args) {
+		float mark;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the marks:");
+		mark=sc.nextInt();
+		if(mark>=90)
+		{
+			System.out.println("S grade");
+		}
+		else if(mark>=80&&mark<90)
+		{
+			System.out.println("A grade");
+			
+		}
+		else if(mark>=70&&mark<80)
+		{
+			System.out.println("B grade");
+			
+		}
+		else if(mark>=60&&mark<70)
+		{
+			System.out.println("C grade");
+			
+		}
+		else if(mark>=50&&mark<60)
+		{
+			System.out.println("D grade");
+			
+		}
+		else 
+		{
+			System.out.println("failed");
+			
+		}
+	}
+	
+
+}
