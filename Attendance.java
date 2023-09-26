219e799d48b9c005c677942e4c20f0fac05ae4ee
naveenajesh1 <145547512+naveenajesh1@users.noreply.github.com>
2023-09-26 15:00:05 +0530

    Add files via upload

---
+++ Attendance.java
@@ -0,0 +1,29 @@
+/*
+ * File        : Attendance.java
+ * Description : Print hello world
+ * Author      : Naveen
+ * Version     : 1.0
+ * Date        : 26/09/2023
+ * 
+ */
+
+package javalab;
+import java.util.Scanner;
+public class Attendance {
+	public static void main(String[] args) {
+	float attendancepercentage;
+	Scanner sc= new Scanner (System.in);
+	System.out.println("Enter the attendance percentage:");
+	attendancepercentage=sc.nextInt();
+	if(attendancepercentage>=90)
+	{
+		System.out.println("Grade is 10");
+	}
+	else
+	{
+		float newmark=attendancepercentage/10;
+		System.out.println("The Grade is "+newmark);
+	}
+	}
+
+}
