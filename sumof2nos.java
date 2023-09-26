c0357cd35be272d5a0dc06202334614eafbe7874
naveenajesh1 <145547512+naveenajesh1@users.noreply.github.com>
2023-09-26 16:13:30 +0530

    Update sumof2nos.java

--- sumof2nos.java
+++ sumof2nos.java
@@ -1,3 +1,12 @@
+/*
+ * File        : sumof2nos.java
+ * Description : Print sum of 2 nos
+ * Author      : Naveen
+ * Version     : 1.0
+ * Date        : 26/09/2023
+ * 
+ */
+
 package javalab;
 import java.util.Scanner;
 public class sumof2nos {
