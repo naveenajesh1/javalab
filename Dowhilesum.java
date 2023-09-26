551bbb36d48186482f78c7cade6ba1664d817f87
naveenajesh1 <145547512+naveenajesh1@users.noreply.github.com>
2023-09-26 16:07:03 +0530

    Add files via upload

---
+++ Dowhilesum.java
@@ -0,0 +1,27 @@
+/*
+ * File        : Whilesum.java
+ * Description : Print hello world
+ * Author      : Naveen
+ * Version     : 1.0
+ * Date        : 26/09/2023
+ * 
+ */
+package javalab;
+import java.util.Scanner;
+public class Dowhilesum {
+	public static void main(String[] args) {
+		int num=0,sum=0;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the nos");
+		num=sc.nextInt();
+		do 
+		{
+			sum=sum+num;
+			num=sc.nextInt();
+		}
+		while(num>0);
+	
+	System.out.println("Sum is "+sum);
+
+}
+}
