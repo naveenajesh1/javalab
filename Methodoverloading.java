f3d03ccc7d26941ade88329ac5524eea5cd8612f
Naveen Ajesh <145547512+naveenajesh1@users.noreply.github.com>
2023-10-10 16:23:14 +0530

    Add files via upload

--- Methodoverloading.java
+++ Methodoverloading.java
@@ -1,18 +1,33 @@
 /*
  * File        : Methodoverloading.java
- * Description : To to calculate the area of different shapes using method overloading
+ * Description : To calculate the area of different shapes using method overloading
  * Author      : Naveen
  * Version     : 1.0
  * Date        : 10/10/2023
  * 
  */
-
+import java.util.Scanner;
 public class Methodoverloading {
  public static void main(String[] args) {
 	 Shape shape=new Shape();
-		Shape.area(12.5f,13.5f);
-		Shape.area(15,30);
-		Shape.area(17.5f);
+		Scanner sc=new Scanner(System.in);
+		System.out.print("enter the base of triangle:");
+		float base=sc.nextFloat();
+		
+		System.out.print("enter the height of triangle:");
+		float height=sc.nextFloat();
+	
+		System.out.print("enter the length of rectangle:");
+		int length=sc.nextInt();
+		
+		System.out.print("enter the breadth of rectangle:");
+		int breadth=sc.nextInt();
+		
+		System.out.print("enter the radius of circle:");
+		float radius=sc.nextFloat();
+		Shape.area(height,base);
+		Shape.area(length,breadth);
+		Shape.area(radius);
 		
 	}
 
@@ -20,12 +35,12 @@ public class Methodoverloading {
 class Shape{
 	
 	public static void area(int length,int breadth) {
-		System.out.println("Area of Rectangle="+(length*breadth));
+		System.out.println("Area of Rectangle with width:"+length+" and breadth:"+breadth+" is:"+(length*breadth));
 	}
 	public static void area(float height,float base) {
-		System.out.println("Area of triangle="+(0.5*height*base));
+		System.out.println("Area of triangle with base:"+base+" and height:"+height+" is:"+(0.5*height*base));
 	}
 	public static void area(float radius) {
-		System.out.println("Area of circle="+(3.14*radius*radius));
+		System.out.println("Area of circle with radius:"+radius+" is:"+(3.14*radius*radius));
 	}
 }
