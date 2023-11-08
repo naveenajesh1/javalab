120158e44a807897e874c6941207bbc09df76457
Naveen Ajesh <145547512+naveenajesh1@users.noreply.github.com>
2023-11-08 09:41:33 +0530

    Add files via upload

---
+++ ExceptionClass.java
@@ -0,0 +1,32 @@
+package w3resource;
+
+public class ExceptionClass {
+	public static void main(String[] args) {
+		
+		try {
+			String s="123";
+			int [] array=new int[5];
+			System.out.println(Integer.parseInt(s));
+			System.out.println(10/0);
+			System.out.println(array[8]);
+		}
+		catch(NumberFormatException e) {
+			//e.printStackTrace();
+			System.out.println("Sorry !! Number format");
+		}
+		catch(ArithmeticException e) {
+			//e.printStackTrace();
+			System.out.println("Sorry !! Arithmetic Exception");
+		}
+		catch(Exception e) {
+			//e.printStackTrace();
+			System.out.println("Unexpected Exception happend");
+		}
+		
+		finally {
+			System.out.println("I am finally block");
+		}
+		
+		
+	}
+}
